9c4e7cedef0253ae72791ef967b9942f31cbd2d0
Almost Engineer <bitsecondal@gmail.com>
2020-12-11 22:55:46 -0600

    Updated handing of settings and documentation

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -25,19 +25,69 @@ namespace Almostengr.FalconPiMonitor.Models
 
     public class AlarmSettings
     {
-        public double Temperature { get; set; }
+        [Required]
+        private double _tempThreshold;
+        public double TempThreshold
+        {
+            get { return _tempThreshold; }
+            set { _tempThreshold = SetTempThreshold(value); }
+        }
         public string TwitterUser { get; set; }
+
+        private double SetTempThreshold(double? threshold)
+        {
+            if (threshold == null || threshold < 0)
+            {
+                threshold = 55.0;
+            }
+            return (double)threshold;
+        }
     }
 
     public class FppMonitorSettings
     {
         public bool PostOffline { get; set; }
-        public int RefreshInterval { get; set; }
+
+        private int _refreshInterval;
+        public int RefreshInterval
+        {
+            get { return _refreshInterval; }
+            set { _refreshInterval = SetRefreshInterval(value); }
+        }
+
+        private int SetRefreshInterval(int? interval)
+        {
+            if (interval == null || interval < 0)
+            {
+                interval = 15;
+            }
+            return (int)interval;
+        }
     }
 
     public class FalconPiPlayerSettings
     {
+        private string _falconPiUri;
         [Required]
-        public string FalconUri { get; set; }
+        public string FalconUri
+        {
+            get { return _falconPiUri; }
+            set { _falconPiUri = SetFalconPiUri(value); }
+        }
+
+        private string SetFalconPiUri(string uri)
+        {
+            uri = uri.ToLower().Replace("api/", "").Replace("api", "");
+
+            if (uri.StartsWith("http://") == false && uri.StartsWith("https://") == false)
+            {
+                uri = string.Concat("http://", uri);
+            }
+
+            uri = string.Concat(uri, "/api/");
+            uri = uri.Replace("//api/", "/api/");
+
+            return uri;
+        }
     }
 }
