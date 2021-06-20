932d8e7ef232260b0b1fb4d0215cb7e81cef51c6
Almost Engineer <bitsecondal@gmail.com>
2021-06-20 17:07:29 -0500

    #5 Updated workflow to tag releases

---
+++ scripts/release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+####################################################################################
+# DESCRIPTION: Commit files and update tags after main branch has been updated.
+# AUTHOR: Kenny Robinson, @almostengr
+# CREATED: 2021-06-20
+####################################################################################
+
+STATUSOUTPUT=$(git status)
+
+if [[ ${STATUSOUTPUT} != *"nothing to commit, working tree clean"* ]]; then
+    echo "Updates were found"
+
+    # git pull
+    git config user.name github-actions
+    git config user.email github-actions@github.com
+    git add release
+    git commit -m "Committed latest release"
+    # git push
+
+    LASTTAG=$(git tag | tail -1)
+    CURRENTYEAR=$(date +%Y)
+
+    if [[ "${LASTTAG}" == *${CURRENTYEAR}* ]]; then
+        NEWTAG=$((${LASTTAG} + 1))
+    else
+        NEWTAG="${CURRENTYEAR}01"
+    fi
+
+    git tag ${NEWTAG}
+
+    git push --tags
+
+else
+    echo "No updates were found"
+    exit 0
+fi
