f4473f0c08bd0e2b85d366a2d6cf8dd4b5ba5a5c
Almost Engineer <bitsecondal@gmail.com>
2021-06-20 22:08:24 -0500

    #5 Added hour and minute to build number

--- Almostengr.FalconPiTwitter/Program.cs
+++ Almostengr.FalconPiTwitter/Program.cs
@@ -85,7 +85,7 @@ namespace Almostengr.FalconPiTwitter
 
         private static void ShowVersion()
         {
-            Console.WriteLine(typeof(Program).Assembly.GetName().Version.ToString(3));
+            Console.WriteLine(typeof(Program).Assembly.GetName().Version.ToString());
         }
 
         private static void ShowHelp()
