7375c3cfacca9ef0a7aafbbc901e9a0015e548ec
Almost Engineer <bitsecondal@gmail.com>
2021-12-10 22:20:42 -0600

    Updated settings properties

---
+++ Almostengr.FalconPiTwitter/AppSettings.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace Almostengr.FalconPiTwitter.DataTransferObjects
+{
+    public class AppSettings
+    {
+        public Twitter Twitter { get; set; }
+        public RaspberryPi RaspberryPi { get; set; }
+        public bool MonitorOnly { get; set; } = false;
+    }
+
+    public class RaspberryPi
+    {
+        public double MaxCpuTemperatureC { get; set; } = 60.0;
+    }
+
+    public class Twitter
+    {
+        public string ConsumerSecret { get; set; }
+        public string ConsumerKey { get; set; }
+        public string AccessToken { get; set; }
+        public string AccessSecret { get; set; }
+        public List<string> AlarmUsers { get; set; } = new();
+    }
+
+}
