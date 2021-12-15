71940b9af18606d5f34c554a0b58b95d9786215b
Almost Engineer <bitsecondal@gmail.com>
2021-12-14 20:07:19 -0600

    Updated constants

--- Almostengr.FalconPiTwitter/Constants.cs
+++ Almostengr.FalconPiTwitter/Constants.cs
@@ -4,7 +4,7 @@ namespace Almostengr.FalconPiTwitter.Constants
 {
     public sealed class TwitterConstants
     {
-        public static readonly int TweetCharacterLimit = 280;
+        public const int TweetCharacterLimit = 280;
         public static readonly string[] ChristmasHashTags = {
             "#LightShow", "#AnimatedLights", "#LedLighting",
             "#ChristmasLightShow", "#ChristmasLights", "#Christmas", "#Christmas", "#ChristmasSeason",
@@ -17,8 +17,8 @@ namespace Almostengr.FalconPiTwitter.Constants
 
     public sealed class PlaylistIgnoreName
     {
-        public static readonly string Offline = "offline";
-        public static readonly string Testing = "testing";
+        public const string Offline = "offline";
+        public const string Testing = "testing";
     }
 
     public sealed class AppConstants
@@ -28,21 +28,21 @@ namespace Almostengr.FalconPiTwitter.Constants
 
     public sealed class FppMode
     {
-        public static readonly string Standalone = "standalone";
-        public static readonly string Master = "master";
-        public static readonly string Remote = "remote";
+        public const string Standalone = "standalone";
+        public const string Master = "master";
+        public const string Remote = "remote";
     }
 
     public sealed class DelaySeconds
     {
-        public static readonly int Short = 15;
-        public static readonly int Long = 300;
-        public static readonly int ExtraLong = 900;
+        public const int Short = 15;
+        public const int Long = 300;
+        public const int ExtraLong = 900;
     }
 
     public sealed class ExceptionMessage
     {
-        public static readonly string NoInternetConnection = "Are you connected to internet? Is FFPPd running? ";
-        public static readonly string NullReference = "Null Exception occurred. ";
+        public const string NoInternetConnection = "Are you connected to internet? Is FPPd running? ";
+        public const string NullReference = "Null Exception occurred. ";
     }
 }
