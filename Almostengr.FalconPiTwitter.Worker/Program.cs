248e5c372f22e0a47cbdf54dedf119e4ba840c81
Almost Engineer <bitsecondal@gmail.com>
2022-06-11 18:06:44 -0500

    Added systemd and content root to builder

--- Almostengr.FalconPiTwitter.Worker/Program.cs
+++ Almostengr.FalconPiTwitter.Worker/Program.cs
@@ -1,13 +1,23 @@
 using Almostengr.FalconPiTwitter.Worker;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    .UseSystemd()
+    .UseContentRoot(
+        System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location))
     .ConfigureServices(services =>
     {
+                    // IConfiguration configuration = hostContext.Configuration;
+                    // services.Configuration()
+
         services.AddHostedService<ChristmasCountDownWorker>();
         services.AddHostedService<FppCurrentSongWorker>();
         services.AddHostedService<FppVitalsWorker>();
         services.AddHostedService<LightShowCountdownWorker>();
+
+        
     })
     .Build();
+    
+Console.WriteLine(typeof(Program).Assembly.ToString());
 
 await host.RunAsync();
