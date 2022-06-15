d342110473404ad3ca878e72f2d77421098f2b7a
Almost Engineer <bitsecondal@gmail.com>
2022-06-14 21:42:20 -0500

    trimmed file

--- Almostengr.FalconPiTwitter.Common/Services/SystemdService.cs
+++ Almostengr.FalconPiTwitter.Common/Services/SystemdService.cs
@@ -5,13 +5,13 @@ namespace Almostengr.FalconPiTwitter.Common.Services
     public class SystemdService : ISystemdService
     {
         private const string FPT_SERVICE_FILE = "/lib/systemd/system/falconpitwitter.service";
-        private const string SYSD_MESSAGE = 
+        private const string SYSD_MESSAGE =
             "This application has not been configured as a system service. See https://thealmostengineer.com/projects/falcon-pi-twitter/#installation-steps for more info.";
         private readonly ILogger<SystemdService> _logger;
 
         public SystemdService(ILogger<SystemdService> logger)
         {
-            _logger =logger;
+            _logger = logger;
         }
 
         public void CheckForSystemdServiceFile()
