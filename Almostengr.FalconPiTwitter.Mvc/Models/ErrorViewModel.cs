abdf044f0898534fb70a10bac1599c005fa5ec1c
Almost Engineer <bitsecondal@gmail.com>
2022-06-17 00:23:54 -0500

    Added web front end project

---
+++ Almostengr.FalconPiTwitter.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Almostengr.FalconPiTwitter.Mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
