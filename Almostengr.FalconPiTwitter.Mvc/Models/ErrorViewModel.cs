261808ac504155ab41cc04646bf8e0b009d9f907
Almost Engineer <bitsecondal@gmail.com>
2022-06-18 16:38:33 -0500

    Removed mvc project files

--- Almostengr.FalconPiTwitter.Mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Almostengr.FalconPiTwitter.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
