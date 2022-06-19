4c57892d083a4435b1a1b0e013cb95d4396a6492
Almost Engineer <bitsecondal@gmail.com>
2022-06-19 08:55:05 -0500

    Removed unused constants

--- Almostengr.FalconPiTwitter.Common/Constants/ExceptionMessage.cs
+++ Almostengr.FalconPiTwitter.Common/Constants/ExceptionMessage.cs
@@ -3,7 +3,6 @@ namespace Almostengr.FalconPiTwitter.Common.Constants
     public sealed class ExceptionMessage
     {
         public const string NoInternetConnection = "Are you connected to internet? Is FPPd running? ";
-        public const string NullReference = "Null Exception occurred. ";
         public const string FppOffline = "FPP did not respond. Is it online?";
         public const string FppFrozen = "FPP appears to be stuck or frozen";
     }
