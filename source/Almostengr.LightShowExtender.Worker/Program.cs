45b8013ff70630fd0d8c6f31fb471963fa59e5c8
Almost Engineer <bitsecondal@gmail.com>
2023-09-03 22:59:50 -0500

    Added config and environemnt

--- source/Almostengr.LightShowExtender.Worker/Program.cs
+++ source/Almostengr.LightShowExtender.Worker/Program.cs
@@ -3,6 +3,24 @@ using Almostengr.LightShowExtender.DomainService.Common;
 using Almostengr.LightShowExtender.Infrastructure.Logging;
 using Almostengr.LightShowExtender.DomainService.Jukebox;
 using Almostengr.LightShowExtender.DomainService.Monitoring;
+using Almostengr.LightShowExtender.DomainService.Common.Constants;
+
+Console.WriteLine(typeof(Program).Assembly.ToString());
+
+string environment = string.Empty;
+
+#if RELEASE
+    environment = AppEnvironment.Prod;
+#else
+environment = AppEnvironment.Devl;
+#endif
+
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile(
+        (environment == AppEnvironment.Prod) ? AppConstants.AppSettingsProdFile : AppConstants.AppSettingsDevlFile,
+        false,
+        true)
+    .Build();
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((hostingContext, config) =>
@@ -11,15 +29,17 @@ IHost host = Host.CreateDefaultBuilder(args)
     })
     .ConfigureServices(services =>
     {
-        services.AddSingleton<AppSettings>(); // todo review for correctness
-
-        services.AddHostedService<MonitoringWorker>();
-        services.AddHostedService<JukeboxWorker>();
+        AppSettings appSettings = configuration.GetSection(nameof(AppSettings)).Get<AppSettings>();
+        services.AddSingleton(appSettings);
 
         services.AddSingleton<IMonitoringService, MonitoringService>();
         services.AddSingleton<IJukeboxService, JukeboxService>();
 
         services.AddSingleton(typeof(ILoggingService<>), typeof(LoggingService<>));
+
+
+        services.AddHostedService<MonitoringWorker>();
+        services.AddHostedService<JukeboxWorker>();
     })
     .UseSystemd()
     .Build();
