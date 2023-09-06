aa9e0f5cec434b1b56648f3197c3c61cb89c7231
Kenny Robinson <bitsecondal@gmail.com>
2023-09-06 16:31:30 -0500

    reorganized classes

--- source/Almostengr.LightShowExtender.DomainService/Common/AppSettings.cs
+++ source/Almostengr.LightShowExtender.DomainService/Common/AppSettings.cs
@@ -3,10 +3,12 @@ namespace Almostengr.LightShowExtender.DomainService.Common;
 public class AppSettings
 {
     public Monitoring Monitoring { get; init; } = new();
-}
+    public string EngineeringUrl { get; init; } = string.Empty;
+    public string FalconPlayerUrl { get; init; } = string.Empty;
 
-public class Monitoring
-{
-    public double MaxCpuTemperatureC { get; init; } = 60.0;
-    public string NwsStationId {get ;init; } = "KMGM";
+    public class Monitoring
+    {
+        public double MaxCpuTemperatureC { get; init; } = 60.0;
+        public string NwsStationId {get ;init; } = "KMGM";
+    }
 }
--- source/Almostengr.LightShowExtender.DomainService/FalconPiPlayer/FppMediaMetaDto.cs
+++ source/Almostengr.LightShowExtender.DomainService/FalconPiPlayer/FppMediaMetaDto.cs
@@ -5,16 +5,16 @@ namespace Almostengr.LightShowExtender.DomainService.FalconPiPlayer;
 public class FppMediaMetaDto : BaseDto
 {
     public FalconMediaMetaFormat Format { get; init; } = new();
-}
 
-public class FalconMediaMetaFormat
-{
-    public FalconMediaMetaFormatTags Tags { get; init; } = new();
-}
+    public class FalconMediaMetaFormat
+    {
+        public FalconMediaMetaFormatTags Tags { get; init; } = new();
 
-public class FalconMediaMetaFormatTags
-{
-    public string Title { get; init; } = string.Empty;
-    public string Artist { get; init; } = string.Empty;
-    public string Album { get; init; } = string.Empty;
+        public class FalconMediaMetaFormatTags
+        {
+            public string Title { get; init; } = string.Empty;
+            public string Artist { get; init; } = string.Empty;
+            public string Album { get; init; } = string.Empty;
+        }
+    }
 }
