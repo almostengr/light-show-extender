7a842419a499cc2ff2a6c84cde9c2b8bec343527
Almost Engineer <bitsecondal@gmail.com>
2023-09-10 16:59:33 -0500

    change for deployment and release via VS code; added system service documentation

--- source/Almostengr.LightShowExtender.DomainService/Common/Constants/AppConstants.cs
+++ source/Almostengr.LightShowExtender.DomainService/Common/Constants/AppConstants.cs
@@ -1,8 +1,8 @@
 namespace Almostengr.LightShowExtender.DomainService.Common.Constants;
 
-public  static class AppConstants
+public static class AppConstants
 {
-    public const string AppSettingsProdFile = "/home/fpp/media/upload/falconpitwitter.json";
+    public const string AppSettingsProdFile = "/home/fpp/media/upload/lightshowextender.appsettings.json";
     public const string AppSettingsDevlFile = "appsettings.Development.json";
     public const int ErrorDelaySeconds = 5;
 }
