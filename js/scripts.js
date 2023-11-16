f1321348dc827d7cec019d649643fe924e296085
Almost Engineer <bitsecondal@gmail.com>
2023-11-15 20:15:38 -0600

    Deployed dc9eeec with MkDocs version: 1.5.1

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,6 @@ const windChillElement = document.getElementById("windChill");
 const lastUpdatedElement = document.getElementById("lastUpdated");
 const errorsElement = document.getElementById("errors");
 const onAirElement = document.getElementById("onAir");
-const stationFrequencyElement = document.getElementById("stationFrequency");
 
 function requestHeaders() {
     return { "Content-Type": "application/json" };
@@ -77,7 +76,6 @@ async function getDisplayData() {
             jukeboxFormElement.classList.add(D_NONE);
             showMetaDataElement.classList.add(D_NONE);
             onAirElement.classList.add(D_NONE);
-            stationFrequencyElement.classList.add(D_NONE);
         }
         else {
             songTitleElement.innerText = result.title;
@@ -89,7 +87,6 @@ async function getDisplayData() {
             jukeboxFormElement.classList.remove(D_NONE);
             showMetaDataElement.classList.remove(D_NONE);
             onAirElement.classList.remove(D_NONE);
-            stationFrequencyElement.classList.remove(D_NONE);
         }
 
         errorsElement.innerText = "";
