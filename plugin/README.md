8aa71ef9fe64d527235cdc4f6335718136a6593c
Almost Engineer <bitsecondal@gmail.com>
2024-01-09 21:07:31 -0600

    Copied the fpp plugin template

--- plugin/README.md
+++ plugin/README.md
@@ -1,3 +1,12 @@
-# fpp-plugin-Template
-Template plugin for FPP Plugin developers
+# Light Show Extender
 
+## More Information
+
+For more information about this project, visit the User Guide folder or
+[project page](https://thealmostengineer.com/projects/light-show-extender).
+
+## References and Guides
+
+Additional information and coding examples that were used 
+
+* https://www.parksq.co.uk/dotnet-core/dependency-injection-httpclient-and-ihttpclientfactory
--- plugin/about.php
+++ plugin/about.php
@@ -1,17 +1,17 @@
-  <div style="margin:0 auto;"> <br />
-    <fieldset style="padding: 10px; border: 2px solid #000;">
-      <legend>Template Plugin Info</legend>
-      <div style="overflow: hidden; padding: 10px;">
-    <div>
-      <div id='credits'>
-        <b>Template Plugin Developed By:</b><br />
-		<br />
-        John Doe (jdoe)<br />
-		<br />
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template'>Git Repository</a><br>
-        <a href='https://github.com/FalconChristmas/fpp-plugin-Template/issues'>Bug Reporter</a><br>
-		<br />
+<div style="margin:0 auto;"> <br />
+  <fieldset style="padding: 10px; border: 2px solid #000;">
+    <legend>Light Show Extender</legend>
+    <div style="overflow: hidden; padding: 10px;">
+      <div>
+        <div id='credits'>
+          <b>Plugin Developed By:</b><br />
+          <br />
+          Kenny Robinson (almostengr)<br />
+          <br />
+          <a href='https://github.com/almostengr/light-show-extender'>Git Repository</a><br>
+          <a href='https://github.com/almostengr/light-show-extender/issues'>Bug Reporter</a><br>
+          <br />
+        </div>
       </div>
-    </div>
-    </fieldset>
-  </div>
+  </fieldset>
+</div>
