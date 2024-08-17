5dc4d604f2f28ec643751dc4dda9e9c23098cd19
Almost Engineer <bitsecondal@gmail.com>
2024-08-17 04:07:38 -0500

    Removed wled project

--- extender/Almostengr.Wled/DomainService/IWledHttpClient.cs
+++
@@ -1,7 +0,0 @@
-namespace Almostengr.Wled.DomainService
-
-public interface IWledHttpClient
-{
-    Task<WledJsonStateResponse> GetStatusAsync(string hostname, CancellationToken cancellationToken);
-    Task<WledJsonStateResponse> PostStateAsync(string hostname, WledJsonStateRequest wledRequestDto, CancellationToken cancellationToken);
-}
