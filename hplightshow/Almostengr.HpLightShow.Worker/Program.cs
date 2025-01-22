c9026721f4284a4f32342fc312daecd721df1bec
almostengr <info@rhtservices.net>
2025-01-21 19:41:16 -0600

    closed #123 added configuration loaded from upload directory

--- hplightshow/Almostengr.HpLightShow.Worker/Program.cs
+++ hplightshow/Almostengr.HpLightShow.Worker/Program.cs
@@ -1,3 +1,4 @@
+using Almostengr.HpLightShow.Core.Common;
 using Almostengr.HpLightShow.Core.Countdowns;
 using Almostengr.HpLightShow.Worker;
 
@@ -5,10 +6,41 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHttpClient();
 
+loadConfiguration(builder);
+
 builder.Services.AddTransient<ICountdownService, CountdownService>();
+
+// builder.Services.AddSingleton(typeof(ILogger<>), typeof(LoggingService<>));
 
 builder.Services.AddHostedService<CountdownWorker>();
 builder.Services.AddHostedService<FppMonitorWorker>();
 
 var host = builder.Build();
 host.Run();
+
+
+
+
+void loadConfiguration(HostApplicationBuilder builder)
+{
+    const string PROD = "prod";
+    string environment = PROD;
+
+    #if !RELEASE
+        environment = "devl";
+    #endif
+
+    builder.Configuration.Sources.Clear();
+
+    IConfiguration configuration = new ConfigurationBuilder()
+        .AddJsonFile(
+            (environment == PROD) ? 
+                "/home/fpp/media/upload/appsettings.json" : 
+                "appsettings.Development.json",
+            false,
+            false)
+        .Build();
+
+    // builder.Configuration.AddConfiguration(configuration);
+    builder.Services.AddSingleton(configuration.GetSection(nameof(HpLightShowAppSettings)));
+}
